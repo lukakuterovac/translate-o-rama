45b8ad5eda418acd68edff787110d85cb0c18387
Luka Kuterovac <luka.kuterovac@gmail.com>
2023-01-30 22:13:11 +0100

    Add sign up view and template

--- translationsite/translationsite/urls.py
+++ translationsite/translationsite/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home_path'),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", views.home, name="home_path"),
 ]
