51db6f27f66b81ba27b226d067482c9f6b94f3c0
tialedencan <tialedencan@gmail.com>
2023-01-31 11:43:02 +0100

    Add dashboard view

--- translationsite/app/views.py
+++ translationsite/app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def home(request):
     context = {}
     return render(request, "app/home.html", context)
+
+
+def dashboard(request):
+    if request.user.is_superuser:
+        return HttpResponseRedirect("", "")
+    return HttpResponse("/")
