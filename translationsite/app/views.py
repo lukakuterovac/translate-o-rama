c9c92e0974e7017dd0ff25cfdd74ffbf4b9c5ddc
janaledencan <janaledencan@gmail.com>
2023-01-31 16:49:20 +0100

    Add job model, make post_job.html

--- translationsite/app/views.py
+++ translationsite/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Job
 
 
 def home(request):
@@ -13,3 +15,19 @@ def dashboard(request):
         "user": user,
     }
     return render(request, "app/dashboard.html", context)
+
+
+def post_job(request):
+    if request.method == "POST":
+        job_post = Job.objects.create(
+            user=request.user,
+            title=request.POST["title"],
+            description=request.POST["description"],
+            source_language=request.POST["source_lang"],
+            target_language=request.POST["target_lang"],
+            job_field=request.POST["job_field"],
+            budget=request.POST["budget"],
+            text=request.POST["text"],
+        )
+        return HttpResponseRedirect(reverse("app:post_job", args=[]))
+    return render(request, "app/post_job.html", {})
